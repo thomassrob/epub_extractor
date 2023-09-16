d216e6e277254374363f03749b79e4110425e167
thomassrob <39812653+thomassrob@users.noreply.github.com>
2023-09-16 12:34:03 -0700

    Get the image and cover files directly from epublib rather than extracting using zip

--- epubImageExt.py
+++ epubImageExt.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
-import tempfile
 from pathlib import Path
 from zipfile import ZipFile
-from ebooklib import epub
+
+import ebooklib
 from bs4 import BeautifulSoup
-import shutil
+from ebooklib import epub
 
 epub_name = sys.argv[1]
 output_dir_name = sys.argv[2]
@@ -30,35 +30,43 @@ epub_base_name = Path(epubFile.filename).stem
 output_dir = output_dir_root / epub_base_name
 Path(output_dir).mkdir()
 
-temp_dir = tempfile.TemporaryDirectory()
-# Path(temp_dir).mkdir()
-
-# debug(book.spine)
-
-# page=book.get_item_with_id("p_0146")
-# print(page)
-# print(content)
-
 imageIdx = 0
+
+
+def get_image_map(src_book: epub) -> dict:
+    images = src_book.get_items_of_type(ebooklib.ITEM_IMAGE)
+    image_map = dict ()
+    for image in images:
+        image_map[image.file_name] = image.id
+    return image_map
+
+
+image_map = get_image_map(book)
 
 for page in book.spine:
     page_id = page[0]
     item = book.get_item_with_id(page_id)
     content = item.get_body_content()
     soup = BeautifulSoup(content, features="lxml")
+
+    covers = book.get_items_of_type(ebooklib.ITEM_COVER)
+    cover_file = open(output_dir_root / epub_base_name / "00-cover.jpg", "wb")
+    cover_file.write(next(covers).get_content())
+    cover_file.close()
+
     for imgtag in soup.find_all('img'):
-        # print("imgtag")
-        # print(imgtag)
-        # print("imgtag src")
-        # print(imgtag['src'])
         rawSrc = imgtag['src']
-        imageSrc = rawSrc.replace("../", "OEBPS/")
-        image_ext = Path(imageSrc).suffix
-        extractedFile = epubFile.extract(imageSrc, temp_dir.name)
-        # print(extractedFile)
+        image_id = image_map.get(rawSrc)
+
+        image_ext = Path(rawSrc).suffix
+        image = book.get_item_with_id(image_id)
+
         output_name = "image%s%s" % (imageIdx, image_ext)
+        image_file = open(output_dir_root / epub_base_name / output_name, "wb")
+        image_file.write(image.get_content())
+        image_file.close()
         imageIdx += 1
         # print(outputName)
-        shutil.move(extractedFile, output_dir_root / epub_base_name / output_name)
+#        shutil.move(extractedFile, output_dir_root / epub_base_name / output_name)
 
-temp_dir.cleanup()
+
